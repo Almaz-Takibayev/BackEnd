b32e6520f228687a2a863b05f43e17f5b1bb44dc
Almaz Takibayev <almaz.takibayev@gmail.com>
2021-02-19 14:55:45 +0600

    Calculator

---
+++ Week 4/src/main/scala/Calculator.scala
@@ -0,0 +1,118 @@
+import scala.io.StdIn
+
+class Calculator {
+  def calculate(): Unit = {
+    var expression = userInput()
+
+    var operations: List[Char] = List()
+    var numbers: List[Double] = List()
+
+    var number = ""
+
+    var hasDot: Boolean = false
+
+    for (i <- 0 to expression.length - 1) {
+      if (isOperation(expression.charAt(i)) && !isOperation(expression.charAt(0))) {
+        if (!isOperation(expression.charAt(i-1))) {
+          operations = operations :+ expression.charAt(i)
+
+          numbers = numbers :+ number.toDouble
+          number = ""
+
+          hasDot = false
+        }
+      }
+      else if (isDigit(expression.charAt(i))) {
+        number += expression.charAt(i)
+      }
+      else if (isElementOfNumber(expression.charAt(i)) && isDigit(expression.charAt(i - 1)) && isDigit(expression.charAt(i + 1)) && !hasDot) {
+        number += expression.charAt(i)
+        hasDot = true
+      }
+      else {
+        println("Your input is not correct!")
+        return 0
+      }
+    }
+
+    var result = getResult(numbers, operations)
+    println("*************************")
+    println(result)
+
+    //    for (n <- numbers) print(n + " ")
+    //    println()
+    //
+    //    for (o <- operations) print(o + " ")
+  }
+
+  def userInput(): String = {
+    var input: String = ""
+    var expression: String = ""
+
+    while (!input.contains('=')) {
+      input = StdIn.readLine();
+      expression += input.replaceAll("\\s", "")
+    }
+
+    return expression
+  }
+
+  def isOperation(c: Char): Boolean = {
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '=') {
+      return true
+    }
+
+    return false
+  }
+
+  def isDigit(c: Char): Boolean = {
+    if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+      return true
+    }
+
+    return false
+  }
+
+  def isElementOfNumber(c: Char): Boolean = {
+    if (c == '.') {
+      return true
+    }
+
+    return false
+  }
+
+  def getResult(numbers: List[Double], operations: List[Char]): Double = {
+    var result = numbers(0)
+
+    for (i <- 1 to numbers.length - 1) {
+      result = operate(result, numbers(i), operations(i-1))
+    }
+
+    return result
+  }
+
+  def operate(result: Double, number: Double, operation: Char): Double = {
+    var res = result
+
+    if (operation == '+') {
+      res += number
+    }
+    else if (operation == '-') {
+      res -= number
+    }
+    else if (operation == '*') {
+      res *= number
+    }
+    else if (operation == '/') {
+      if (number != 0) {
+        res /= number
+      }
+      else {
+        System.out.println("You can not divide to 0!")
+        return 0
+      }
+    }
+
+    return res
+  }
+}
---
+++ Week 4/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import scala.io.StdIn
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val calculator = new Calculator
+    calculator.calculate()
+  }
+}
